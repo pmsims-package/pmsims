c903ef37383a3eeadf9e482fa569472de2c1d333
Gordon Forbes <gordonjohnforbes@gmail.com>
2023-03-03 16:59:46 +0000

    Changing the plotting function to plot a median rather than a mean
    I also changed this in the linear extrapolation function.
    
    This means it will play nice with quantile regression.
    
    We may want something more flexible here.

--- R/calculate_sample_size.R
+++ R/calculate_sample_size.R
@@ -174,19 +174,19 @@ plot_sample_size_curve <- function(results_summaries) {
   train_size <- results_summaries$train_size
   performance <- results_summaries$summaries
   quant_performance <- performance$quant20_performance
-  mean_performance <- performance$mean_performance
+  median_performance <- performance$median_performance
   quant5_performance <- performance$quant5_performance
   quant95_performance <- performance$quant95_performance
 
   # Plot
   plot(train_size, quant_performance, type = "l" , lty = 2, col = "red",
        main = "AUC by train size with  20th percintile, 5th percentile, & 95th percentile")
-  graphics::lines(train_size, mean_performance, col = "black")
+  graphics::lines(train_size, median_performance, col = "black")
   graphics::lines(train_size, quant5_performance, col = "grey", lty = 1)
   graphics::lines(train_size, quant95_performance, col = "grey", lty = 1)
   #abline(h = target_auc, col = 3)
   graphics::abline(h = results_summaries$target, col = "green", lty = 3)
   graphics::abline(v = results_summaries$min_n, col = "green", lty = 3)
-  graphics::legend("bottomright", legend = c("AUC mean", "AUC 5th to 95th percentile", "AUC 20th percentile", "Acceptable AUC"),
+  graphics::legend("bottomright", legend = c("AUC median", "AUC 5th to 95th percentile", "AUC 20th percentile", "Acceptable AUC"),
          col = c("black", "grey", "red", "green"), lty = c(1,1,2,3))
 }
