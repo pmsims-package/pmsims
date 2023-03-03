691e79ca52b36183a33b22610d8642ffeeba8ef2
Gordon Forbes <gordonjohnforbes@gmail.com>
2023-03-03 16:57:48 +0000

    Creating separate script for surrogate models
    -Rename process results function sm_linear_extrapolation and moving into a separate surrogate models script.

--- R/calculate_sample_size.R
+++ R/calculate_sample_size.R
@@ -149,29 +149,6 @@ run_simulations <- function(simulation_parameters,
                             performance_function = performance_function,
                             tune_param = tune_param) {
 
-process_results <- function(results, simulation_parameters, target_performance) {
-  mean_performance  <-  apply(results, FUN = mean, MARGIN = 1, na.rm= TRUE)
-  quant20_performance <- apply(results, FUN = stats::quantile, MARGIN = 1, probs = 0.2, na.rm = TRUE)
-  quant5_performance  <-  apply(results, FUN = stats::quantile, MARGIN = 1, probs = 0.05, na.rm = TRUE)
-  quant95_performance  <-  apply(results, FUN = stats::quantile, MARGIN = 1, probs = 0.95, na.rm = TRUE)
-
-  # min training size where 80% of AUC are > 0.70:
-  # We linearly approximate values between the train sizes and get the min_n from there:
-  min_size_func = stats::approxfun(quant20_performance,
-                                   simulation_parameters$train_size,
-                                   method = "linear")
-  min_n = min_size_func(target_performance)
-
-  return_list <- list(min_n = min_n,
-                      target = target_performance,
-                      summaries = data.frame(
-                                    mean_performance = mean_performance,
-                                    quant20_performance = quant20_performance,
-                                    quant5_performance = quant5_performance,
-                                    quant95_performance = quant95_performance),
-                      data = results,
-                      train_size = simulation_parameters$train_size)
-  return(return_list)
   results <- matrix(nrow = length(simulation_parameters$train_size),
          ncol = max(simulation_parameters$n_sims))
   for (i in 1:length(simulation_parameters$train_size)) {
