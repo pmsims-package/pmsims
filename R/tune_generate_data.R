1e7c183b9e681b45b8d26f062137095455da554d
Gordon Forbes <gordonjohnforbes@gmail.com>
2023-04-21 16:09:46 +0100

    Adding code to expand range if initial interval is too small
    This includes a while loop and a counter to exit the while loop if more than 10 reps are run.

--- R/tune_generate_data.R
+++ R/tune_generate_data.R
@@ -5,14 +5,16 @@ tune_generate_data <- function(data_generating_function,
                                model_function,
                                performance_function,
                                target_large_sample_performance,
-                               tolerance = target_large_sample_performance / 100) {
+                               tolerance = target_large_sample_performance / 100,
+                               max_interval_expansion = 10) {
   
-  
+  interval = c(min_tune_arg, max_tune_arg)
   
   # Optimise
-  optimal_param <- stats::optimise(optimise_me, # function defined below
-    lower = min_tune_arg,
-    upper = max_tune_arg,
+  
+  result <- stats::optimise(
+    optimise_me, # function defined below
+    interval = interval,
     maximum = FALSE,
     tol = tolerance,
     n = large_n,
@@ -21,7 +23,39 @@ tune_generate_data <- function(data_generating_function,
     performance_function = performance_function,
     target_large_sample_performance = target_large_sample_performance
   )
-  return(optimal_param$minimum)
+  optimal_value <- result$minimum
+  expand_count <- 1
+  # Expand range if solution is close to limits
+  while (abs(optimal_value - interval[1]) < max(abs(optimal_value - interval[1]))/100 || 
+         abs(optimal_value - interval[2])  < max(abs(optimal_value - interval[1]))/100  ||
+         result$objective > tolerance) {
+    # Interval is too narrow, expand the interval
+    
+    print("Expanding search for tuning parameter")
+    expand_count <- expand_count + 1
+    if (expand_count > max_interval_expansion) {
+      stop("cannot find interval containing tuning parameter")
+    } 
+    interval <- c(interval[1]-1, interval[2]+1)
+    
+    # Call the optimise function with the expanded interval
+    result <- stats::optimise(
+       optimise_me, # function defined below
+       interval = interval,
+       maximum = FALSE,
+       tol = tolerance,
+       n = large_n,
+       data_generating_function = data_generating_function,
+       model_function = model_function,
+       performance_function = performance_function,
+       target_large_sample_performance = target_large_sample_performance
+    )
+    
+    # Extract the optimal value and its corresponding objective function value
+    optimal_value <- result$minimum
+  }
+  
+  return(result$minimum )
 }
 
 
