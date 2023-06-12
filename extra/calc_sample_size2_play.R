c9a00e46188284a2494411aad87c79807336fcae
Gordon Forbes <gordonjohnforbes@gmail.com>
2023-06-12 10:03:47 +0100

    editing file for playing around with our function

--- extra/calc_sample_size2_play.R
+++ extra/calc_sample_size2_play.R
@@ -1,14 +1,25 @@
 set.seed(123)
-sample_size <-  pmsims::calculate_sample_size2(data = list(type = "binary"),
-                                      large_sample_performance = 0.8,
-                                      target_performance = 0.78,
-                                      min_sample_size = 100,
-                                      max_sample_size = 3000,
-                                      n_reps = 100,
-                                      test_n = 10000)
+sample_size <-  pmsims::calculate_sample_size2(
+  data = list(type = "binary"),
+  large_sample_performance = 0.8,
+  target_performance = 0.78,
+  min_sample_size = 100,
+  max_sample_size = 3000,
+  n_reps = 100,
+  test_n = 10000
+)
 print(sample_size$min_n)
 
-
+sample_size <-  pmsims::calculate_sample_size2(
+  data = list(type = "binary"),
+  tune_param = 0.7,
+  target_performance = 0.7,
+  min_sample_size = 100,
+  max_sample_size = 3000,
+  n_reps = 100,
+  test_n = 10000
+)
+print(sample_size$min_n)
 
 
 set.seed(123)
@@ -31,3 +42,10 @@ sample_size3 <-  pmsims::calculate_sample_size2(data = list(type = "binary"),
 print(sample_size3$min_n)
 
 
+
+data_options <- list(type = "binary",
+                     args = list(n_params = 30))
+data_generating_function <- default_data_generators(data_options)
+m <- default_model_generators(type = "binary")
+
+tune_generate_data
