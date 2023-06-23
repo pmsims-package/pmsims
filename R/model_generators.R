77dfc85af654a3b912181f766e952d534e933099
Diana <bigdatawithdiana@gmail.com>
2023-06-23 19:38:33 +0100

    adding metrics
    The below default metric functions are chosen depending on the data outcome (binary/continuous/survival) and metrics (auc, calib_slope, brier_score, brier_score_scaled for binary, auc or cindex for survival,r2 for continuous)
    The following metric functions are assigned depending on the inputs:
    survival_auc
    survival_cindex
    continuous_r2
    binary_brier_score_scaled
    binary_brier_score
    binary_calib_slope
    binary_auc_metric

--- R/model_generators.R
+++ R/model_generators.R
@@ -11,26 +11,14 @@ default_model_generators <- function(outcome) {
     model <- function(data) {
       logistic_model <- glm("y ~ .", data = data, family = "binomial")
     }
-
-    # Get performance must be a function of data and a model object
-    metric <- function(data, model) {
-      y <- data[, 1]
-      x <- data[, -1]
-      y_hat <- predict(model, x, type = "response")
-      auc <- pROC::auc(y, as.numeric(y_hat), quiet = TRUE)
-      return(auc[1])
-    }
   } else if (opts$outcome == "linear") {
-    f <- function() {
-      return(data.frame())
+    model <- function(data) {
+      linear_model <- glm("y ~ .", data = data, family = "gaussian")
     }
   } else if (opts$outcome == "survival") {
-    f <- function(params = parameters,
-                  }
-
-    return(list(
-      model = model,
-      metric = metric
-    ))
-  }
+    model <- function(data) {
+      cox_model <- survival::cox.ph("Surv(time, event) ~ .", data = data)
+      }
+    }
+  return(list(model = model))
 }
