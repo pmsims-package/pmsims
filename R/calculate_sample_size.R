895e8d543e3f769ff3f30362f2cd7fddaa49cfba
Ewan Carr <ewan.carr@kcl.ac.uk>
2023-07-03 17:46:27 +0000

    Add check for large_sample_performance

--- R/calculate_sample_size.R
+++ R/calculate_sample_size.R
@@ -57,6 +57,11 @@ simulate_custom <- function(data_function = NULL,
   if (min_sample_size > max_sample_size) {
     stop("min_sample_size must be less than max_sample_size")
   }
+  
+  if (!is.null(large_sample_performance)) {
+    large_sample_performance <- max(large_sample_performance,
+                                    min(max(10000, 50 * signal_parameters), 100000))
+  }
 
   # Set tuning arguments
   if (is.null(tune_param)) {
