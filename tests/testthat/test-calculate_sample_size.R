297caa10ccc8e5841a318f5c04e5f8428a417095
GForb <48352959+GForb@users.noreply.github.com>
2025-01-28 11:13:40 +0000

    Moving test for parse_inputs into test-simulate_custom file
    We moved parse_inputs into its own file so this matches the tests file structure to the files

--- tests/testthat/test-calculate_sample_size.R
+++ tests/testthat/test-calculate_sample_size.R
@@ -1,114 +1,65 @@
-  test_that("parse_inputs", {
-    # Binary
-    inputs <- parse_inputs(
-      data_spec = list(
-        type = "binary",
-        args = list(
-          signal_parameters = 5,
-          noise_parameters = 5,
-          predictor_type = "continuous",
-          beta_signal = 0.1,
-          baseline_prob = 0.1
-        )
-      ),
-      metric = "auc"
-    )
-    expect_equal(length(inputs), 3)
+test_that("simulate_binary", {
+  set.seed(1234)
 
-    # Continuous
+  output <- simulate_binary(
+    signal_parameters = 5,
+    noise_parameters = 5,
+    predictor_type = "continuous",
+    baseline_prob = 0.1,
+    min_sample_size = 100,
+    max_sample_size = 3000,
+    n_reps_total = 50,
+    minimum_threshold = 0.05,
+    tune_param = 0.60
+  )
+  expect_equal(length(output), 8)
+})
 
-    inputs <- parse_inputs(
-      data_spec = list(
-        type = "continuous",
-        args = list(
-          signal_parameters = 5,
-          noise_parameters = 5,
-          predictor_type = "continuous",
-          beta_signal = 0.1
-        )
-      ),
-      metric = "r2"
-    )
-    expect_equal(length(inputs), 3)
+test_that("simulate_binary_many_metrics", {
+  set.seed(1234)
+  output <- simulate_binary_many_metrics(
+    signal_parameters = 5,
+    noise_parameters = 5,
+    predictor_type = "continuous",
+    baseline_prob = 0.1,
+    min_sample_size = 100,
+    max_sample_size = 3000,
+    n_reps_total = 50,
+    large_sample_auc = NULL,
+    tune_param = -0.599631,
+    target_performance = c(0.75, 0.9)
+  )
+  expect_equal(length(output), 16)
+})
 
-    # Survival
-    inputs <- parse_inputs(
-      data_spec = list(
-        type = "survival",
-        args = list(
-          signal_parameters = 5,
-          noise_parameters = 5,
-          predictor_type = "continuous",
-          beta_signal = 0.1
-        )
-      ),
-      metric = "auc"
-    )
-    expect_equal(length(inputs), 3)
-  })
-
-  test_that("simulate_binary", {
-    set.seed(1234)
-
-    output <- simulate_binary(
-      signal_parameters = 5,
-      noise_parameters = 5,
-      predictor_type = "continuous",
-      baseline_prob = 0.1,
-      min_sample_size = 100,
-      max_sample_size = 3000,
-      n_reps_total = 50,
-      minimum_threshold = 0.05,
-      tune_param = 0.60
-    )
-    expect_equal(length(output), 8)
-  })
-
-  test_that("simulate_binary_many_metrics", {
-    set.seed(1234)
-    output <- simulate_binary_many_metrics(
-      signal_parameters = 5,
-      noise_parameters = 5,
-      predictor_type = "continuous",
-      baseline_prob = 0.1,
-      min_sample_size = 100,
-      max_sample_size = 3000,
-      n_reps_total = 50,
-      large_sample_auc = NULL,
-      tune_param = -0.599631,
-      target_performance = c(0.75, 0.9)
-    )
-    expect_equal(length(output), 16)
-  })
-
-  test_that("simulate_continuous", {
-    set.seed(4321)
-    output <- simulate_continuous(
-      signal_parameters = 10,
-      noise_parameters = 10,
-      predictor_type = "continuous",
-      min_sample_size = 100,
-      max_sample_size = 3000,
-      n_reps_total = 10,
-      minimum_threshold = 0.05,
-      tune_param = 0.6353973
-    )
-    expect_equal(length(output), 8)
-  })
+test_that("simulate_continuous", {
+  set.seed(4321)
+  output <- simulate_continuous(
+    signal_parameters = 10,
+    noise_parameters = 10,
+    predictor_type = "continuous",
+    min_sample_size = 100,
+    max_sample_size = 3000,
+    n_reps_total = 10,
+    minimum_threshold = 0.05,
+    tune_param = 0.6353973
+  )
+  expect_equal(length(output), 8)
+})
 
 
-  # test_that("simulate_survival", {
-  #   set.seed(1234)
-  #
-  #   output <- simulate_survival(
-  #     signal_parameters = 10,
-  #     noise_parameters = 10,
-  #     predictor_type = "continuous",
-  #     min_sample_size = 100,
-  #     max_sample_size = 3000,
-  #     n_reps_total = 10,
-  #     minimum_threshold = 0.05,
-  #     large_sample_performance = 0.7
-  #   )
-  #   expect_equal(length(output), 8)
-  # })
+# test_that("simulate_survival", {
+#   set.seed(1234)
+#
+#   output <- simulate_survival(
+#     signal_parameters = 10,
+#     noise_parameters = 10,
+#     predictor_type = "continuous",
+#     min_sample_size = 100,
+#     max_sample_size = 3000,
+#     n_reps_total = 10,
+#     minimum_threshold = 0.05,
+#     large_sample_performance = 0.7
+#   )
+#   expect_equal(length(output), 8)
+# })
