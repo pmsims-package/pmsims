b3688297eca4e855a942b6eee52ad031ec342515
GForb <48352959+GForb@users.noreply.github.com>
2025-01-28 12:19:19 +0000

    Adding model names to parse_inputs test

--- tests/testthat/test-simulate_custom.R
+++ tests/testthat/test-simulate_custom.R
@@ -11,7 +11,8 @@ test_that("parse_inputs", {
         baseline_prob = 0.1
       )
     ),
-    metric = "auc"
+    metric = "auc",
+    model = "glm"
   )
   expect_equal(length(inputs), 3)
   
@@ -21,13 +22,14 @@ test_that("parse_inputs", {
     data_spec = list(
       type = "continuous",
       args = list(
-        signal_parameters = 5,
+        n_signal_parameters = 5,
         noise_parameters = 5,
         predictor_type = "continuous",
         beta_signal = 0.1
       )
     ),
-    metric = "r2"
+    metric = "r2",
+    model = "lm"
   )
   expect_equal(length(inputs), 3)
   
@@ -42,7 +44,9 @@ test_that("parse_inputs", {
         beta_signal = 0.1
       )
     ),
-    metric = "auc"
+    metric = "auc",
+    model = "coxph"
+    
   )
   expect_equal(length(inputs), 3)
 })
