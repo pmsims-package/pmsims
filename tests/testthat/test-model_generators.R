4d4486be64ff41f9f2254edd398718b79facc13b
GForb <48352959+GForb@users.noreply.github.com>
2025-01-28 12:20:24 +0000

    Adding model argument and changing n_signal_parameters argument

--- tests/testthat/test-model_generators.R
+++ tests/testthat/test-model_generators.R
@@ -1,7 +1,7 @@
 test_that("cox_model", {
   inputs <- parse_inputs(data_spec = list(type = "survival",
                                           args = list(
-                                            signal_parameters = 5, 
+                                            n_signal_parameters = 5, 
                                             noise_parameters = 5, 
                                             predictor_type = "continuous", 
                                             beta_signal = 0.1,
@@ -9,7 +9,8 @@ test_that("cox_model", {
                                             censoring_rate = 0.2
                                           )
   ),
-  metric="auc")
+  metric="auc",
+  model = "coxph")
                          
   data <- inputs$data_function(1000)
   test_data <- inputs$data_function(1000)
