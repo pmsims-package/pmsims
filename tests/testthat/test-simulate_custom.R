862bfb794d5d664a5fbdb9f4721a6bd4d3c72627
GForb <48352959+GForb@users.noreply.github.com>
2025-02-04 12:23:44 +0000

    Adding test for simulate custom

--- tests/testthat/test-simulate_custom.R
+++ tests/testthat/test-simulate_custom.R
@@ -50,3 +50,76 @@ test_that("parse_inputs", {
   )
   expect_equal(length(inputs), 3)
 })
+
+test_that("simulate_custom", {
+  set.seed(1234)
+  data_opts <- list(type = "binary", 
+                    args = list(
+                      n_signal_parameters = 5,
+                      noise_parameters = 5,
+                      predictor_type = "continuous",
+                      baseline_prob = 0.2
+                    ))
+  data_function <- default_data_generators(data_opts)
+  outcome_type <- attr(data_function, "outcome")
+  model_function <- default_model_generators(outcome_type, model = "glm")
+  
+  metric_function = default_metric_generator(
+    "auc",
+    data_function
+  )
+  
+  tuning_parameter <- default_tune(tune_param = beta_signal, 
+                                   max_sample_size = 10000,
+                                   large_sample_performance = 0.8,
+                                   data_function = data_function,
+                                   model_function = model_function,
+                                   metric_function = metric_function)
+  tuned_data_opts <- list(type = "binary", 
+                    args = list(
+                      n_signal_parameters = 5,
+                      noise_parameters = 5,
+                      predictor_type = "continuous",
+                      baseline_prob = 0.2,
+                      beta_signal = tuning_parameter
+                    ))
+  
+  tuned_data_function <- default_data_generators(tuned_data_opts)
+  
+  
+  sim_results_mlpwr <- simulate_custom(data_function = tuned_data_function,
+                              model_function = model_function,
+                              metric_function = metric_function,
+                              target_performance = 0.75,
+                              test_n = 10000,
+                              min_sample_size = 75,
+                              max_sample_size = 500,
+                              n_reps_total = 100,
+                              n_reps_per = 10,
+                              se_final = NULL,
+                              n_init = 4,
+                              method = "mlpwr",
+                              verbose = FALSE)
+  
+  expect_equal(length(sim_results_mlpwr), 8 )
+  expect_equal(sim_results_mlpwr$min_n, 130, tolerance = 20)
+  
+  sim_results_crude <- simulate_custom(data_function = tuned_data_function,
+                                       model_function = model_function,
+                                       metric_function = metric_function,
+                                       target_performance = 0.75,
+                                       test_n = 10000,
+                                       min_sample_size = 75,
+                                       max_sample_size = 500,
+                                       n_reps_total = 100,
+                                       n_reps_per = 10,
+                                       se_final = NULL,
+                                       n_init = 4,
+                                       method = "crude",
+                                       verbose = FALSE)
+  
+  expect_equal(length(sim_results_crude), 8 )
+  expect_equal(sim_results_crude$min_n, 130, tolerance = 20)
+  
+})
+  
